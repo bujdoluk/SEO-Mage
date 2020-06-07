af0c8d376c69e80ff7cdb4a4e3287ea534d73647
bujdoluk <lukas.bujdos@gmail.com>
2020-06-07 18:08:19 +0200

    Update 1.2

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-$(document).ready(function () {
-
-    $(window).scroll(function () {
-        console.log($(window).scrollTop())
-        if ($(window).scrollTop() > 280) {
-            $('#nav_bar').addClass('nav_bar');
-        }
-        if ($(window).scrollTop() < 281) {
-            $('#nav_bar').removeClass('nav_bar');
-        }
-    });
-});
+window.addEventListener("scroll", function () {
+    var nav = this.document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0);
+})
